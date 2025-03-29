97369421d2cb7bdfb126ec3c6b99081da934e749
Dashuai Yue <2338853081@qq.com>
2025-03-29 23:44:33 +0800

    3.29 [for / while]

---
+++ src/main/java/org/example/algorithms/linkedlist/TestBase.java
@@ -0,0 +1,42 @@
+package org.example.algorithms.linkedlist;
+
+public class TestBase {
+    public static void main(String[] args) {
+
+
+        int i = 0;
+//        // 只执行一次 初始化
+//        // 条件过了 才可以 进入循环体
+//        // 循环体结束后 执行 迭代后处理
+//        for (i = 0; i < 2; System.out.println("Hello, World!")) {
+//            i++;
+//        }
+
+
+
+
+//
+//        for (i = 8; i < 10; ) {
+//            i++;
+//            System.out.println("Hello, World!");
+//        }
+
+
+//
+//        for (i = 8; i < 10; i++) {
+//            System.out.println("Hello, World!");
+//        }
+
+//        for (int i = 8; i < 10; System.out.println("Hello, World!"), i++)
+//        {
+//
+//        }
+
+//        int j = 0;
+//        while (j < 3) {
+//            System.out.println("Hello, World!");
+//            j++;
+//        }
+
+    }
+}
