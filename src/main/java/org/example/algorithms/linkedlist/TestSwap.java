2174babc5df390c6f600690580cffc923559d70c
Owen <chenkeyu1226@126.com>
2025-03-29 22:15:20 +0000

    3.29 [for / while]

--- src/main/java/org/example/algorithms/linkedlist/TestSwap.java
+++ src/main/java/org/example/algorithms/linkedlist/TestSwap.java
@@ -9,6 +9,9 @@ public class TestSwap {
         // int a;
         // a = 5;
         int b = 6;
+        swapInt(a, b);
+        System.out.println(a);
+        System.out.println(b);
 
         int[] arr = {1, 2, 3, 4, 5}; //new了一个int类型数组 并给它赋值
         int i = 0;
@@ -24,11 +27,6 @@ public class TestSwap {
         System.out.println(Arrays.deepToString(arr2)); //打印二维数组
         swapTwoArr(arr2, i, j);
         System.out.println(Arrays.deepToString(arr2)); //打印二维数组
-
-//        changeInt(a,b);
-        // int a_copy = a;
-        // int b_copy = b;
-
 
 
     }
