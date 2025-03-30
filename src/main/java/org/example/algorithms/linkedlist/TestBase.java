781152e3637facbe679ab189d3c349ca4035078a
Owen <chenkeyu1226@126.com>
2025-03-30 15:12:58 +0100

    3.29 [for / while]

--- src/main/java/org/example/algorithms/linkedlist/TestBase.java
+++ src/main/java/org/example/algorithms/linkedlist/TestBase.java
@@ -1,5 +1,7 @@
 package org.example.algorithms.linkedlist;
 
+import javafx.beans.binding.When;
+
 public class TestBase {
     public static void main(String[] args) {
 
@@ -8,31 +10,59 @@ public class TestBase {
 //        // 只执行一次 初始化
 //        // 条件过了 才可以 进入循环体
 //        // 循环体结束后 执行 迭代后处理
-        for (i = 0; i < 2; System.out.println("Hello, World!")) {
+//        for (i = 0; i < 2; System.out.println("Hello, World!")) {
+//            i++;
+//        }
+
+        while (i<2) {
+            i++;
+            System.out.println("Hello, World");
+        }
+
+
+
+
+//
+//        for (i = 8; i < 10; ) {
+//            i++;
+//            System.out.println("Hello, World!");
+//        }
+
+
+        while (i<10) {
+            i++;
+            System.out.println("Hello, World");
+        }
+
+
+//
+//        for (i = 8; i < 10; i++) {
+//            System.out.println("Hello, World!");
+//        }
+
+
+        while (i<10) {
+            System.out.println("Hello, World");
             i++;
         }
 
+//        for (int i = 8; i < 10; System.out.println("Hello, World!"), i++)
+//        {
+//
+//        }
 
-        for (i = 8; i < 10; ) {
+        while (i<10) {
+            System.out.println("Hello, World");
             i++;
-            System.out.println("Hello, World!");
         }
 
+//        int j = 0;
+//        while (j < 3) {
+//            System.out.println("Hello, World!");
+//            j++;
+//        }
 
-        for (i = 8; i < 10; i++) {
-            System.out.println("Hello, World!");
-        }
 
-        for (int x = 8; x < 10; x++) {
-            System.out.println("Hello, World!");
-
-        }
-
-        int j = 0;
-        while (j < 3) {
-            System.out.println("Hello, World!");
-            j++;
-        }
 
     }
 }
