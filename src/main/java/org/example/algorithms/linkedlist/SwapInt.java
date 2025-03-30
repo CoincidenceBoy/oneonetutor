ce4330cdc559237c8b645243419d8ded4ce8f77e
Owen <chenkeyu1226@126.com>
2025-03-30 14:24:53 +0100

    3.29 [for / while]

--- src/main/java/org/example/algorithms/linkedlist/SwapInt.java
+++ src/main/java/org/example/algorithms/linkedlist/SwapInt.java
@@ -2,15 +2,16 @@ package org.example.algorithms.linkedlist;
 
 public class SwapInt {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        System.out.println("(" + a + ", " + b + ")");
+        int a = 5;
+        int b = 6;
+        System.out.println(a);
+        System.out.println(b);
 
         // 通过临时变量直接交换
         int temp = a;
         a = b;
         b = temp;
-
-        System.out.println("(" + a + ", " + b + ")");
+        System.out.println(a);
+        System.out.println(b);
     }
 }
