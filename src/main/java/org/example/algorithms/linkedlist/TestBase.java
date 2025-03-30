cb09cb0bc51888ec4938a24b4a50a3aae7712597
Owen <chenkeyu1226@126.com>
2025-03-30 15:04:29 +0100

    3.29 [for / while]

--- src/main/java/org/example/algorithms/linkedlist/TestBase.java
+++ src/main/java/org/example/algorithms/linkedlist/TestBase.java
@@ -8,35 +8,31 @@ public class TestBase {
 //        // 只执行一次 初始化
 //        // 条件过了 才可以 进入循环体
 //        // 循环体结束后 执行 迭代后处理
-//        for (i = 0; i < 2; System.out.println("Hello, World!")) {
-//            i++;
-//        }
+        for (i = 0; i < 2; System.out.println("Hello, World!")) {
+            i++;
+        }
 
 
+        for (i = 8; i < 10; ) {
+            i++;
+            System.out.println("Hello, World!");
+        }
 
 
-//
-//        for (i = 8; i < 10; ) {
-//            i++;
-//            System.out.println("Hello, World!");
-//        }
+        for (i = 8; i < 10; i++) {
+            System.out.println("Hello, World!");
+        }
 
+        for (int x = 8; x < 10; x++) {
+            System.out.println("Hello, World!");
 
-//
-//        for (i = 8; i < 10; i++) {
-//            System.out.println("Hello, World!");
-//        }
+        }
 
-//        for (int i = 8; i < 10; System.out.println("Hello, World!"), i++)
-//        {
-//
-//        }
-
-//        int j = 0;
-//        while (j < 3) {
-//            System.out.println("Hello, World!");
-//            j++;
-//        }
+        int j = 0;
+        while (j < 3) {
+            System.out.println("Hello, World!");
+            j++;
+        }
 
     }
 }
